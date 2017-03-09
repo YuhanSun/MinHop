08d9c640fa3e16f28b603d8e8cb553660b8d1fd6
Yuhan Sun <wdmzssyh@gmail.com>
2017-03-08 19:47:53 -0700

    reorganize

--- DataPreprocess.h
+++ DataPreprocess.h
@@ -12,6 +12,12 @@ void ReadGraph(vector<set<int>> &graph, string filepath);
 
 void OutputGraph(vector<set<int>> &graph, string filepath);
 
+/**
+ * convert from directional to bi-directional
+ * make sure if a-->b exists then b-->a exists as well
+ * @param ori_graph_path
+ * @param new_graph_path
+ */
 void GraphConvert(string ori_graph_path, string new_graph_path);
 
 
--- main.cpp
+++ main.cpp
@@ -1,104 +1,10 @@
 #include <iostream>
 #include "Universe.h"
 #include "DataPreprocess.h"
+#include "Hmbr.h"
+#include "Experiment.h"
 
 #define TIME_RECORD
-
-/**
- * brutal force very slow, 100 hrs for yelp
- * @param graph
- * @param entities
- * @param hop_num
- * @param HMBR
- */
-void GenerateHMBR_BF(vector<vector<int>> &graph, vector<Entity> &entities, int hop_num, vector<vector<MyRect>> &HMBR)
-{
-    HMBR.resize(entities.size());
-    for ( int i = 0; i < graph.size(); i++)
-    {
-        HMBR[i].resize(hop_num);
-
-        vector<int> prehop_neighbors;
-        vector<int> curhop_neighbors;
-        prehop_neighbors.reserve(entities.size());
-        curhop_neighbors.reserve(entities.size());
-
-        prehop_neighbors.push_back(i);
-        for ( int j = 0; j < hop_num; j++)
-        {
-            vector<bool> visited = vector<bool>(entities.size());
-            for ( int k = 0; k < prehop_neighbors.size(); k++)
-            {
-                int pre_id = prehop_neighbors[k];
-                for ( int l = 0; l < graph[pre_id].size(); l++)
-                {
-                    int pre_neighbor_id = graph[pre_id][l];
-                    if(visited[pre_neighbor_id])
-                        continue;
-                    curhop_neighbors.push_back(pre_neighbor_id);
-                    visited[pre_neighbor_id] = true;
-                }
-            }
-
-            int k = 0;
-            MyRect cur_MBR = MyRect();
-            for ( ; k < curhop_neighbors.size(); k++)
-            {
-                Entity p_entity = entities[curhop_neighbors[k]];
-                if(p_entity.IsSpatial)
-                {
-                    cur_MBR = MyRect(p_entity.location.x, p_entity.location.y, p_entity.location.x, p_entity.location.y);
-                    k++;
-                    break;
-                }
-            }
-            for ( ; k < curhop_neighbors.size(); k++)
-            {
-                Entity p_entity = entities[curhop_neighbors[k]];
-                if(p_entity.IsSpatial)
-                    cur_MBR.MBR(p_entity.location);
-            }
-
-            HMBR[i][j] = cur_MBR;
-            prehop_neighbors = curhop_neighbors;
-            curhop_neighbors.clear();
-        }
-
-        cout << i << endl;
-        for ( int j = 0; j < hop_num; j++)
-            cout <<HMBR[i][j].left_bottom.x << "," << HMBR[i][j].left_bottom.y << "," << HMBR[i][j].right_top.x << "," << HMBR[i][j].right_top.y<<endl;
-        cout << endl;
-    }
-}
-
-void GenerateHMBR(vector<vector<int>> &graph, vector<Entity> &entities, int hop_num, vector<vector<MyRect>> &HMBR)
-{
-    HMBR.resize(entities.size());
-    for ( int i = 0; i < graph.size(); i++)
-        HMBR[i].resize(hop_num);
-
-    //initialize HMBR[i][0]
-    for ( int i = 0; i < graph.size(); i++)
-        for ( int j = 0; j < graph[i].size(); j++)
-        {
-            int neighbor = graph[i][j];
-            if(entities[neighbor].IsSpatial)
-                HMBR[i][0].MBR(entities[neighbor].location);
-        }
-
-    //initialize HMBR[i][1]~[hop_num-1]
-    for ( int i = 1; i < hop_num; i++)
-    {
-        for ( int j = 0; j < graph.size(); j++)
-        {
-            for ( int k = 0; k < graph[j].size(); k++)
-            {
-                int neighbor = graph[j][k];
-                HMBR[j][i].MBR(HMBR[neighbor][i-1]);
-            }
-        }
-    }
-}
 
 void OutputHMBR(string output_path, vector<vector<MyRect>> &HMBR, int hop_num)
 {
@@ -120,6 +26,18 @@ void OutputHMBR(string output_path, vector<vector<MyRect>> &HMBR, int hop_num)
     fclose(stdout);
 }
 
+
+
+string dataset = "foursquare";
+
+void DataPreprocess()
+{
+    string dir = "D:\\Ubuntu_shared\\GeoMinHop\\data\\" + dataset;
+    string ori_graph_path = dir + "\\graph_entity_newformat.txt";
+    string new_graph_path = dir + "\\graph.txt";
+    GraphConvert(ori_graph_path,new_graph_path);
+}
+
 void test()
 {
     vector<int> v1 = vector<int>(3);
@@ -137,7 +55,7 @@ void test()
 
 void GenerateHMBR()
 {
-    string dir = "D:\\Ubuntu_shared\\GeoMinHop\\data\\Yelp";
+    string dir = "D:\\Ubuntu_shared\\GeoMinHop\\data\\" + dataset;
     int node_count;
     int hop_num = 6;
 
@@ -150,23 +68,18 @@ void GenerateHMBR()
     ReadEntity(node_count, entities, entity_path);
 
     vector<vector<MyRect>> HMBR;
-    GenerateHMBR(graph, entities, hop_num, HMBR);
+    vector<int> time = GenerateHMBR(graph, entities, hop_num, HMBR);
     OutputHMBR(dir + "\\HMBR.txt", HMBR, hop_num);
 
+    cout << dataset << ": " << time[0] << endl;
     cout << "success"<< endl;
 }
 
-void DataPreprocess()
-{
-    string dataset = "Gowalla";
-    string dir = "D:\\Ubuntu_shared\\GeoMinHop\\data\\" + dataset;
-    string ori_graph_path = dir + "\\graph_entity_newformat.txt";
-    string new_graph_path = dir + "\\graph.txt";
-    GraphConvert(ori_graph_path,new_graph_path);
-}
 
 int main()
 {
-    DataPreprocess();
-//    GenerateHMBR();
+//    DataPreprocess();
+    GenerateHMBR();
+//    IniTime("Gowalla");
+//    IniTime("foursquare");
 }
