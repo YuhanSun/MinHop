439a8f3c365e0ed88d5a1cec556dacbab8820294
Yuhan Sun <wdmzssyh@gmail.com>
2017-03-08 19:45:28 -0700

    record hmbr ini time

---
+++ Experiment.h
@@ -0,0 +1,8 @@
+//
+// Created by ysun138 on 3/8/2017.
+//
+
+#ifndef MINHOP_EXPERIMENT_H
+#define MINHOP_EXPERIMENT_H
+
+#endif //MINHOP_EXPERIMENT_H
---
+++ Hmbr.h
@@ -0,0 +1,8 @@
+//
+// Created by ysun138 on 3/8/2017.
+//
+
+#ifndef MINHOP_HMBR_H
+#define MINHOP_HMBR_H
+
+#endif //MINHOP_HMBR_H
